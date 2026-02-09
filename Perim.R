59dcca4742618c20409fd22566d0aa0f4ff965de
Carmela Lorente García <carloga8@uv.es>
2026-02-09 23:06:59 +0100

    Perimetro rectangulo y con el pi cambiado a numero

--- Perim.R
+++ Perim.R
@@ -1,3 +1,7 @@
 # Perimetro de un circulo 
 
-perim_circulo <- function(r) {return(2*pi*r)}
+perim_circulo <- function(r) {return(2*3.14159*r)}
+
+# Perimetro de un rectangulo
+perim_rectangulo <- function(L, l) {return(2*L+2*l)}
+
